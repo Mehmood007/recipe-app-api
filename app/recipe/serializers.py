143b5429eca02fe5bea78f3a78d893de779b5501
Mehmood Hassan Shah <mehmoodhassan595@yahoo.com>
2024-06-22 10:02:07 -0400

    ingredients feature implemented

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -2,8 +2,17 @@
 Serializers for recipe APIs
 '''
 
-from core.models import Recipe, Tag
+from core.models import Ingredient, Recipe, Tag
 from rest_framework import serializers
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    '''Serializer for ingredients.'''
+
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name']
+        read_only_fields = ['id']
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -19,10 +28,19 @@ class RecipeSerializer(serializers.ModelSerializer):
     '''Serializer for recipes.'''
 
     tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        fields = [
+            'id',
+            'title',
+            'time_minutes',
+            'price',
+            'link',
+            'tags',
+            'ingredients',
+        ]
         read_only_fields = ['id']
 
     def _get_or_create_tags(self, tags, recipe):
@@ -37,11 +55,23 @@ class RecipeSerializer(serializers.ModelSerializer):
 
         return recipe
 
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        '''Handle getting or creating ingredients as needed.'''
+        auth_user = self.context['request'].user
+        for ingredient in ingredients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredient,
+            )
+            recipe.ingredients.add(ingredient_obj)
+
     def create(self, validated_data):
         '''Create a recipe.'''
         tags = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@
 Views for the recipe APIs
 '''
 
-from core.models import Recipe, Tag
+from core.models import Ingredient, Recipe, Tag
 from rest_framework import mixins, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -34,18 +34,34 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(
+class BaseRecipeAttrViewSet(
+    mixins.DestroyModelMixin,
     mixins.UpdateModelMixin,
     mixins.ListModelMixin,
-    mixins.DestroyModelMixin,
     viewsets.GenericViewSet,
 ):
+    '''Base viewset for recipe attributes.'''
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
     '''Manage tags in the database.'''
 
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        '''Filter queryset to authenticated user.'''
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    '''Manage ingredients in the database.'''
+
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
 
     def get_queryset(self):
         '''Filter queryset to authenticated user.'''
